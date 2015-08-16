39119c98539e7961dbbcc25b8e8a28d2593cd1a2
Chris Ward <cward@redhat.com>
2015-08-16 15:34:28 +0200

    do not declare utf encoding for _version; exec(__version__) in setup.py crashes

--- members/_version.py
+++ members/_version.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 # Author: "Chris Ward" <cward@redhat.com>
 
-from __future__ import unicode_literals
+# PY3 COMPAT
+from __future__ import unicode_literals, absolute_import
 
 version_info = ('0', '0', '2')
 __version__ = '.'.join(version_info[0:3])  # + '-' + version_info[3]
