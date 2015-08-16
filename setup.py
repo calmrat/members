7338d77b78950a8955ab9f564604d7201eaf43e8
Chris Ward <cward@redhat.com>
2015-08-16 15:27:13 +0200

    remove pytest-pep8 dep

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ __irequires__ = [
     'pandas',  # FIXME: shouldn't be a hard dep; orgchart3 only
     # FIXME: should be optional...
     'pytest',
-    'pytest-pep8',  # run with `py.test --pep8 ...`
+    # 'pytest-pep8',  # run with `py.test --pep8 ...`
 ]
 pip_src = 'https://pypi.python.org/packages/src'
 __deplinks__ = []
